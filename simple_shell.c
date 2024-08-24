32056009702ee204f95c56d236643cfccb1cec66
elkomysara <sa1244@fayoum.edu.eg>
2024-08-24 14:00:54 +0300

    alx-low_level_programming/alx/simple_shell0-putchar

---
+++ simple_shell.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* External declaration of environ */
+extern char **environ;
+
+/**
+* main - Entry point for the simple shell
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
+char *argv[2];
+
+while (1)
+{
+/* Display the prompt */
+printf("#cisfun$ ");
+
+/* Read the input line */
+nread = getline(&line, &len, stdin);
+if (nread == -1)
+{
+if (feof(stdin))
+{
+/* Handle EOF (Ctrl+D) */
+printf("\n");
+break;
+}
+perror("getline");
+continue;
+}
+
+/* Remove the newline character from the input */
+line[strcspn(line, "\n")] = '\0';
+
+/* Prepare the arguments for execve */
+argv[0] = line;
+argv[1] = NULL;
+
+/* Execute the command */
+if (execve(argv[0], argv, environ) == -1)
+{
+perror("./shell");
+}
+}
+
+/* Free the allocated memory */
+free(line);
+return (0);
+}
